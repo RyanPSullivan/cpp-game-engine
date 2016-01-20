4ff3a85de52bfb9ea20a7bbd21fd27c81273d1b0
Ryan Sullivan <ryan.philip.sullivan@gmail.com>
2016-01-20 21:21:35 +0000

    - include ducktape and update test page to execute hardcoded javascript - start integrating Tup build system, em++ works g++ still requires changes

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <SDL/SDL.h>
+#include "duktape/duktape.h"
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
+
 void one_iter() {
   // process input
   // render to screen
 	printf("WHA");
 }
+
 extern "C" int main(int argc, char** argv) {
   printf("hello, world!\n");
 
@@ -41,7 +44,16 @@ extern "C" int main(int argc, char** argv) {
 
   SDL_Quit();
 
-emscripten_set_main_loop(one_iter, 60, 1);
+  duk_context *ctx = duk_create_heap_default();
+  duk_eval_string(ctx, "print('22+20');");
+  duk_destroy_heap(ctx);
+
+#ifdef __EMSCRIPTEN__
+   emscripten_set_main_loop(one_iter, 60, 1);
+#endif
+
+
+
   return 0;
 }
 
