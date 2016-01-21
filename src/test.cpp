cfa18ea043772df78f6707b2122db66b59b0aabe
Ryan Sullivan <ryan@bossalien.com>
2016-01-21 17:48:34 +0000

    More work on getting dual compilers working

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <SDL/SDL.h>
+
 #include "duktape/duktape.h"
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#include <SDL/SDL.h>
 #endif
 
 void one_iter() {
@@ -15,35 +16,6 @@ void one_iter() {
 extern "C" int main(int argc, char** argv) {
   printf("hello, world!\n");
 
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
-
-#ifdef TEST_SDL_LOCK_OPTS
-  EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
-#endif
-
-  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
-  for (int i = 0; i < 256; i++) {
-    for (int j = 0; j < 256; j++) {
-#ifdef TEST_SDL_LOCK_OPTS
-      // Alpha behaves like in the browser, so write proper opaque pixels.
-      int alpha = 255;
-#else
-      // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
-      // data (and testing that it does get discarded)
-      int alpha = (i+j) % 255;
-#endif
-      *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
-    }
-  }
-  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
-  SDL_Flip(screen); 
-
-  printf("you should see a smoothly-colored square - no sharp lines but the square borders!\n");
-  printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
-
-  SDL_Quit();
-
   duk_context *ctx = duk_create_heap_default();
   duk_eval_string(ctx, "print('22+20');");
   duk_destroy_heap(ctx);
@@ -51,8 +23,6 @@ extern "C" int main(int argc, char** argv) {
 #ifdef __EMSCRIPTEN__
    emscripten_set_main_loop(one_iter, 60, 1);
 #endif
-
-
 
   return 0;
 }
