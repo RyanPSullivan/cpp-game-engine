d7e661d95df9d0118abe36333de136c18195ae2a
Ryan Sullivan <ryan@bossalien.com>
2016-01-25 18:12:19 +0000

    javascript can now be specified on the command line

--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,39 @@
 #include <stdio.h>
-
+#include <string>
 #include "duktape/duktape.h"
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
+#include <emscripten/bind.h>
 #include <SDL/SDL.h>
+#endif
+
+void executeJS( std::string thing )
+{
+  duk_context *ctx = duk_create_heap_default();
+  duk_eval_string(ctx, thing.c_str());
+  duk_destroy_heap(ctx);
+}
+
+#ifdef __EMSCRIPTEN__
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(what)
+{
+   function("executeJS", &executeJS);
+}
 #endif
 
 void one_iter() {
   // process input
   // render to screen
-	printf("WHA");
+    printf("WHA\n");
 }
 
 extern "C" int main(int argc, char** argv) {
   printf("hello, world!\n");
 
-  duk_context *ctx = duk_create_heap_default();
-  duk_eval_string(ctx, "print('22+20');");
-  duk_destroy_heap(ctx);
+  executeJS( "print('22+20');" );
 
 #ifdef __EMSCRIPTEN__
    emscripten_set_main_loop(one_iter, 60, 1);
@@ -26,5 +41,7 @@ extern "C" int main(int argc, char** argv) {
 
   return 0;
 }
+
+
 
 
